aef388850b614f98ac0642b5d46ac587f4416b4a
yosuke1114 <ks0002404@gmail.com>
2021-04-21 10:53:12 +0900

    20210421_2

--- nodecli/main.js
+++ nodecli/main.js
@@ -17,14 +17,37 @@
 const program = require("commander");
 // fsモジュールをfsオブジェクトとしてインポートする
 const fs = require("fs");
+const md2html = require("./md2html");
+//markedモジュールをmarkedオブジェクトとしてインポート
+//const marked = require("marked");
+
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 
 // コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
 const filePath = program.args[0];
 
+// コマンドライン引数のオプションを取得し、デフォルトのオプションを上書きする
+const cliOptions = {
+    gfm: false,
+    ...program.opts(),
+};
+
 // ファイルをUTF-8として非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8"},(err, file) => {
-    console.log(file);
-})
+   if (err) {
+       console.error(err.message);
+       process.exit(1);
+       return;
+   }
+//    //gfmオプションを無効にする
+//    const html = marked(file, {
+//        gfm: cliOptions.gfm,
+//    });
+//     console.log(html);
+    const html = md2html(file, cliOptions);
+    console.log(html);
+});
 
 
